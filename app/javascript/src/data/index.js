639e61b11dc0e6817f2b7597886915faefb52f53
Ubaid Chalil <c.ubaid@gmail.com>
2022-12-05 20:54:07 +0530

    Added notes dummy data

---
+++ app/javascript/src/data/index.js
@@ -0,0 +1,54 @@
+const notes = [
+  {
+    id: 1,
+    title: "Reusable components",
+    description:
+      "In React, a reusable component is a piece of UI that can be used in various part of an application to build more than one UI instance.",
+    assignedContact: { id: 1, name: "Jimmy Hern" },
+    tags: ["Getting Started"],
+    status: "Created",
+    updatedAt: "2022-12-05 17:24:43",
+  },
+  {
+    id: 2,
+    title: "What is CRUD?",
+    description:
+      "In the software development world, CRUD consists of four operations - create, read, update, and delete.",
+    assignedContact: { id: 2, name: "Jack Paul" },
+    tags: ["Getting Started"],
+    status: "Created",
+    updatedAt: "2022-12-05 17:24:43",
+  },
+  {
+    id: 3,
+    title: "What is a slug?",
+    description:
+      "A slug is the part of a URL that identifies a page in human-readable keywords.",
+    assignedContact: { id: 3, name: "Arun Mdhav" },
+    tags: ["Getting Started"],
+    status: "Created",
+    updatedAt: "2022-12-05 17:24:43",
+  },
+  {
+    id: 4,
+    title: "Rails Models",
+    description:
+      "Models are Ruby classes that can be hold the value of a single row in a database table.",
+    assignedContact: { id: 1, name: "Jimmy Hern" },
+    tags: ["Getting Started"],
+    status: "Created",
+    updatedAt: "2022-12-05 17:24:43",
+  },
+  {
+    id: 5,
+    title: "What is CircleCI?",
+    description:
+      "CircleCI is a modern continuous integration and continuous delivery (CI/CD) platform.",
+    assignedContact: { id: 2, name: "Jack paul" },
+    tags: ["Getting Started"],
+    status: "Created",
+    updatedAt: "2022-12-05 17:24:43",
+  },
+];
+
+export { notes };
