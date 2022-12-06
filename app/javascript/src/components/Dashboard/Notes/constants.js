f3dce888ef8301a1bdc215b8e7f55c63c2b1d17a
Ubaid Chalil <c.ubaid@gmail.com>
2022-12-06 16:47:34 +0530

    Added side menu bar for note list screen

--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -9,3 +9,25 @@ export const NOTES_FORM_VALIDATION_SCHEMA = yup.object().shape({
   title: yup.string().required("Title is required"),
   description: yup.string().required("Description is required"),
 });
+
+export const NOTES_MENU_ITEMS = {
+  filterTypes: [
+    { label: "All", dataCount: 13, active: true },
+    { label: "Users", dataCount: 2 },
+    { label: "Leads", dataCount: 7 },
+    { label: "Visitors", dataCount: 4 },
+  ],
+  segments: [
+    { label: "Europe", dataCount: 80 },
+    { label: "Middle-East", dataCount: 75 },
+    { label: "Asia", dataCount: 50 },
+  ],
+  tags: [
+    { label: "Getting Started", dataCount: 40 },
+    { label: "Onboarding", dataCount: 55 },
+    { label: "User Flow", dataCount: 65 },
+    { label: "UX", dataCount: 55 },
+    { label: "Bugs", dataCount: 45 },
+    { label: "V2", dataCount: 75 },
+  ],
+};
--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -5,49 +5,62 @@ import { Button } from "neetoui";
 import { Container, Header } from "neetoui/layouts";
 
 import EmptyState from "components/Common/EmptyState";
+import SideMenuBar from "components/Common/SideMenuBar";
 import { notes as noteList } from "data";
 
 import Card from "./Card";
+import { NOTES_MENU_ITEMS } from "./constants";
 
 const Notes = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [notes, setNotes] = useState([]);
+  const [showSideMenuBar, setShowSideMenuBar] = useState(true);
 
   useEffect(() => {
     setNotes(noteList);
   }, []);
 
   return (
-    <Container>
-      <Header
-        title="All Notes"
-        actionBlock={
-          <Button icon="ri-add-line" label="Add note" size="small" />
-        }
-        searchProps={{
-          value: searchTerm,
-          placeholder: "Search Name, Email and Phone Number",
-          onChange: e => setSearchTerm(e.target.value),
-        }}
+    <>
+      <SideMenuBar
+        menuData={NOTES_MENU_ITEMS}
+        showSideMenuBar={showSideMenuBar}
+        title="Notes"
       />
-      {notes.length > 0 ? (
-        notes.map(note => (
-          <Card
-            {...note}
-            assignedContact={note.assigned_contact}
-            key={note.id}
-            updatedAt={note.updated_at}
-          />
-        ))
-      ) : (
-        <EmptyState
-          image={EmptyNotesListImage}
-          primaryActionLabel="Add note"
-          subtitle="Add your notes to send customized emails to them."
-          title="Looks like you don't have any notes!"
+      <Container>
+        <Header
+          title="All Notes"
+          actionBlock={
+            <Button icon="ri-add-line" label="Add note" size="small" />
+          }
+          menuBarToggle={() =>
+            setShowSideMenuBar(_showSideMenuBar => !_showSideMenuBar)
+          }
+          searchProps={{
+            value: searchTerm,
+            placeholder: "Search Name, Email and Phone Number",
+            onChange: e => setSearchTerm(e.target.value),
+          }}
         />
-      )}
-    </Container>
+        {notes.length > 0 ? (
+          notes.map(note => (
+            <Card
+              {...note}
+              assignedContact={note.assigned_contact}
+              key={note.id}
+              updatedAt={note.updated_at}
+            />
+          ))
+        ) : (
+          <EmptyState
+            image={EmptyNotesListImage}
+            primaryActionLabel="Add note"
+            subtitle="Add your notes to send customized emails to them."
+            title="Looks like you don't have any notes!"
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
