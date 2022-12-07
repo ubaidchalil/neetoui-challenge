8f97643053499f83bae6e4dfac7624411d3702b8
Ubaid Chalil <c.ubaid@gmail.com>
2022-12-07 18:19:46 +0530

    Added header for contact list screen

--- app/javascript/src/components/Dashboard/Contacts/index.jsx
+++ app/javascript/src/components/Dashboard/Contacts/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 import EmptyListImage from "images/EmptyList";
-import { Container } from "neetoui/layouts";
+import { Button } from "neetoui";
+import { Container, Header } from "neetoui/layouts";
 
 import EmptyState from "components/Common/EmptyState";
 import { CONTACTS } from "components/constants";
@@ -10,6 +11,7 @@ import Table from "./Table";
 
 const Notes = () => {
   const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     setContacts(CONTACTS);
@@ -17,12 +19,23 @@ const Notes = () => {
 
   return (
     <Container>
+      <Header
+        title="Contacts"
+        actionBlock={
+          <Button icon="ri-add-line" label="Add Contact" size="small" />
+        }
+        searchProps={{
+          value: searchTerm,
+          placeholder: "Search Name, Email and Phone Number",
+          onChange: e => setSearchTerm(e.target.value),
+        }}
+      />
       {contacts.length > 0 ? (
         <Table contacts={contacts} />
       ) : (
         <EmptyState
           image={EmptyListImage}
-          primaryActionLabel="Add contacts"
+          primaryActionLabel="Add Contacts"
           subtitle="Add your contacts and notes to them."
           title="Looks like you don't have any contacts!"
         />
